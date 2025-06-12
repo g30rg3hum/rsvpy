36ac5d41cb2bcea7b70411c08d5cedb54179e4c0
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2025-06-12 15:55:51 +0100

    added authorisation for deleting attendees

--- src/app/api/events/[id]/attendees/[attendeeId]/route.ts
+++ src/app/api/events/[id]/attendees/[attendeeId]/route.ts
@@ -6,10 +6,47 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: Promise<{ id: string; attendeeId: string }> }
 ) {
-  await authoriseSession();
-
   const { id, attendeeId } = await params;
 
+  // generic login authorisation check
+  const authResponse = await authoriseSession();
+
+  // unauthorised response
+  if (authResponse instanceof Response) {
+    return authResponse;
+  } else {
+    // check that user is the event creator
+    const email = authResponse;
+
+    try {
+      const event = await prisma.event.findUnique({
+        where: { id: id },
+        include: { creator: true },
+      });
+
+      if (!event) {
+        return new Response("Event not found", { status: 404 });
+      }
+
+      // check if user is the event creator
+      if (event.creator.email !== email) {
+        return new Response("Unauthorized to delete attendee", { status: 403 });
+      }
+    } catch (error) {
+      console.error(
+        "Error fetching event while trying to delete attendee:",
+        error
+      );
+      return new Response(
+        "Failed to fetch event while trying to delete attendee",
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+
+  // authorised to delete attendee
   try {
     const { count } = await prisma.eventAttendee.deleteMany({
       where: {
