3272e63affea897c613a50e612a4a0492008d097
Weng Wong (George) Hum <124537582+wengwongg@users.noreply.github.com>
2025-07-02 18:22:10 +0100

    more deployment fixes

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -3,11 +3,11 @@
 
 // const resend = new Resend(process.env.EMAIL_SERVER_PASSWORD);
 
-// export async function POST() {
-//   await resend.emails.send({
-//     from: process.env.EMAIL_FROM || "",
-//     to: "hmw.geo@gmail.com",
-//     subject: "Pay up fool",
-//     react: PayUpEmail(),
-//   });
-// }
+export async function POST() {
+  //   await resend.emails.send({
+  //     from: process.env.EMAIL_FROM || "",
+  //     to: "hmw.geo@gmail.com",
+  //     subject: "Pay up fool",
+  //     react: PayUpEmail(),
+  // });
+}
